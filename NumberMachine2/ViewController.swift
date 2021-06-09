fe2855f7b1ab65b030e6140c19be1d104196bc44
keng-yu liao <g.y.liao81@gmail.com>
2021-06-09 21:29:55 +0800

    Initial Commit

---
+++ NumberMachine2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NumberMachine2
+//
+//  Created by liao on 2021/6/9.
+//  Copyright © 2021 liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
