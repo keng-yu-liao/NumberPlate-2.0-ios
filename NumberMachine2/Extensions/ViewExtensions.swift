f9df013e64bd0ecc8445a60c3fe600724cc489c3
keng-yu liao <g.y.liao81@gmail.com>
2021-06-24 17:58:19 +0800

    [新增] 登入頁面

---
+++ NumberMachine2/Extensions/ViewExtensions.swift
@@ -0,0 +1,53 @@
+//
+//  ViewExtensions.swift
+//  NumberMachine2
+//
+//  Created by liao on 2021/6/23.
+//  Copyright © 2021 liao. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+}
+
+extension UIView {
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
