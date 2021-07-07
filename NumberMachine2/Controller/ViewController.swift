86da5d45b39336832c912dc1022dd58f48f0fcdd
keng-yu liao <g.y.liao81@gmail.com>
2021-07-07 08:17:32 +0800

    [新增] Framework, 登入按鍵機制

--- NumberMachine2/Controller/ViewController.swift
+++ NumberMachine2/Controller/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tfLogin: UITextField!
+    @IBOutlet var btnLogin: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginBtnClick(_ sender: Any) {
+        let alertController = UIAlertController(title: "提示", message: "請輸入用戶名", preferredStyle: UIAlertController.Style.alert)
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        if (tfLogin.text == "") {
+            present(alertController, animated: true, completion: nil)
+        }
+    }
 }
 
